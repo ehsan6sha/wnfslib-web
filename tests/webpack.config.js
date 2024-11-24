2b4def1ca436f44197cd6e4c05f41739a3dba83f
ehsan <ehsan6sha@gmail.com>
2024-11-24 01:35:53 -0500

    added tests

--- tests/webpack.config.js
+++ tests/webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    experiments: {
-        asyncWebAssembly: true
-    },
-    mode: 'development'
+    compress: true,
+    port: 8080,
+  },
 };
